f4c22ced824d7a95509efa06555313788c6d432b
EnzoSylvestrin <enzospavani@gmail.com>
2022-12-22 17:14:22 -0300

    Finalizando função de copia

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { useRef, useState } from 'react';
 import { Button, CopyButton, HomeContainer, SorteioWrapper } from '../styles/HomeStyled'
 
+import copy from "copy-to-clipboard"; 
+
 import { BiCopy } from 'react-icons/bi';
 import { AiOutlineCheck } from 'react-icons/ai';
 
@@ -16,7 +18,7 @@ export default function Home() {
   const p5 = useRef<HTMLParagraphElement>(null);
   const p6 = useRef<HTMLParagraphElement>(null);
 
-  const copy = useRef<HTMLDivElement>(null);
+  const Copy = useRef<HTMLDivElement>(null);
 
   const timerValue = 200;
   const REPEAT_COUNT = 8;
@@ -25,13 +27,14 @@ export default function Home() {
     return new Promise(resolve => setTimeout(resolve, ms || timerValue));
   }
 
-  const Copy = async () => {
-    let element = copy.current;
+  const CopyClick = async () => {
+    let element = Copy.current;
     element?.classList.add('copied');
     setIcon(<AiOutlineCheck />)
     await sleep(2000);
     element?.classList.remove('copied');
-    setIcon(<BiCopy />)
+    setIcon(<BiCopy />);
+    copy(ConcatP());
   }
 
   const SortNumbers = async (e : any) => {
@@ -42,6 +45,29 @@ export default function Home() {
     await ChangeNumbers(p4.current != null ? p4.current : undefined);
     await ChangeNumbers(p5.current != null ? p5.current : undefined);
     await ChangeNumbers(p6.current != null ? p6.current : undefined);
+  }
+
+  const ConcatP = () : string => {
+    let finalStr = '';
+    if (p1.current != null) {
+      finalStr += p1.current.innerHTML;
+    }
+    if (p2.current != null) {
+      finalStr += p2.current.innerHTML;
+    }
+    if (p3.current != null) {
+      finalStr += p3.current.innerHTML;
+    }
+    if (p4.current != null) {
+      finalStr += p4.current.innerHTML;
+    }
+    if (p5.current != null) {
+      finalStr += p5.current.innerHTML;
+    }
+    if (p6.current != null) {
+      finalStr += p6.current.innerHTML;
+    }
+    return finalStr; // for some reason the ternary operator does not work here
   }
 
   const ChangeNumbers = async (element? : HTMLParagraphElement) => {
@@ -78,7 +104,7 @@ export default function Home() {
           <p ref={p4}>4</p>
           <p ref={p5}>5</p>
           <p ref={p6}>6</p>
-          <CopyButton ref={copy} onClick={Copy}>
+          <CopyButton ref={Copy} onClick={CopyClick}>
             {icon}
           </CopyButton>
         </SorteioWrapper>
